48dde5109314a22319e8791813aa9d345e45dbcb
LouiIII3 <jikdsunghuilee@naver.com>
2024-11-01 17:30:34 +0900

    feat:setup project

---
+++ client/pages/index.js
@@ -0,0 +1,48 @@
+// pages/index.js
+import { useState, useEffect } from "react";
+
+export default function Home() {
+  const [users, setUsers] = useState([]);
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const response = await fetch("http://52.79.161.94/api/users");
+    const data = await response.json();
+    setUsers(data);
+  };
+
+  const createUser = async () => {
+    const response = await fetch("http:52.79.161.94/api/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name }),
+    });
+    if (response.ok) {
+      fetchUsers();
+    }
+  };
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        placeholder="Enter user name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={createUser}>Add User</button>
+    </div>
+  );
+}
